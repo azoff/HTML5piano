f86f8619c69ccfb092120ac8a1a32dd0b19efa82
azoff <jazoff@gmail.com>
2010-04-12 02:13:44 -0700

    Improved efficiency by removing extra loops and computation from the rendering cycle for the canvas. Also forced a more uniform method for pressing and releasing keys.

--- piano.js
+++ piano.js
@@ -4,99 +4,212 @@
  *  http://azoffdesign.com/piano
  *
  * Copyright 2010, Jonathan Azoff
- *  Freely distributable contigent to the terms of the DBAD license:
+ *  Freely distributable under the terms outlined in the DBAD license:
  *  http://github.com/SFEley/candy/blob/master/LICENSE.markdown
  *
- * Date: Sunday, April 11th 2010
+ * Date: Sunday, April 12th 2010
  */
 
 /*jslint onevar: true, strict: true, browser: true */
 /*global window, Audio */
-"use strict"; 
-
-(function(_private, _public) {
-
+"use strict";
+(function(_public, _private){
+	
+	/**
+	 * This is the public class for accessing the piano element
+	 * @version 1.0.0
+	 * @requires Audio, HtmlCanvasElement
+	 */
 	_public.Piano = {
 		
 		/**
-		 * A set of constants intended to be used at the argument to the default Piano factory, createInstance. These
-		 * constants define a set of instrument voices the Piano instance can be bound to.
+		 * A collection of piano voices, intended for use with the factory Piano.createInstance
 		 */
-		Voice: {
-
+		Voices: {
+			
 			/**
-			 * A grand piano
+			 * Represents a grand piano
 			 */
-			GRAND_PIANO: "grandPiano" 
-
+			GRAND_PIANO: "grandPiano"
 		},
 		
 		/**
-		 * The audio type that the current browser supports for HTML5 Audio Elements.
-		 * @see http://html5doctor.com/native-audio-in-the-browser/
+		 * The primary factory for creating instances of the Piano class
+		 * @param {String} instrument A string representing the voice to use for the current Piano instance. The selected 	 	  
+		 *                            value should be one of the voices provided in Piano.Voices
 		 */
-		audioType: _private.getAudioType(),
-		
-		/**
-		 * The factory for creating tangible DOM instances of the Piano.
-		 * @param {String} instrument The name of the instrument voice to use for the generated instance of the Piano.
-		 * @returns An instance of the Piano bound to a particular instrument voice. 
-		 * @public 
-		 * @constructor
-		 */
-		createInstance: function(instrument, canvas) {
-			if(this.audioType) {
-				canvas = _private.getPianoCanvas();
-				canvas.audio = _private.loadAudio(instrument, this.audioType, function(){
-					canvas.addEventListener("mousedown", function(event){
-						canvas.mouse = { x: event.clientX-7, y: event.clientY-7 };
-						canvas.drawPiano();
-					}, true);
-					canvas.addEventListener("mouseup", function(event){
-						canvas.mouse = null;
-						canvas.drawPiano();
-					}, true);
-					canvas.addEventListener("mouseover", function(){
-						canvas.active = true;
-						canvas.drawPiano();
-					}, true);
-					canvas.addEventListener("mouseout", function(){
-						canvas.mouse = null;
-						canvas.active = false;
-						canvas.drawPiano();
-					}, true);
-					_public.addEventListener("keydown", function(event, key){
-						key = event.which.toString();
-						if(canvas.active && canvas.keymap.hasOwnProperty(key)) {
-							key = canvas.keymap[key];
-							canvas.keyTargets[key] = true;
-							canvas.press(key);
-							canvas.drawPiano();
-						}
-					}, true);
-					_public.addEventListener("keyup", function(event, key){
-						key = event.which.toString();
-						if(canvas.keymap.hasOwnProperty(key)) {
-							key = canvas.keymap[key];
-							if(canvas.keyTargets[key]) {
-								canvas.keyTargets[key] = false;
-								canvas.release(key);
-								canvas.drawPiano();
-							}
-						}
-					}, true); 
-					canvas.drawPiano();
-				});
-				return canvas;
-			}
+		createInstance: function(instrument, extension) {
+			extension = _private.Piano.getAudioExtension();
+			return extension ? _private.Piano(instrument, extension) : null;
 		}
 		
 	};
-		
-})({
 	
-	// maps ascii keycodes to notes
-	keymap: {
+	_private.Piano = function(instrument, extension, canvas) {
+		canvas 				= document.createElement("canvas");
+		canvas.style.width	= canvas.style.height = "100px";
+		canvas.width 		= canvas.height = 100;
+		canvas.mouseTarget	= null;
+		canvas.pianoBg		= new _private.Piano.Background();
+		canvas.pianoPower	= new _private.Piano.Power();
+		canvas.pianoTitle	= new _private.Piano.Title();
+		canvas.drawPiano	= _private.Piano.drawPiano;
+		canvas.pianoKeys	= _private.Piano.Key.getKeys(instrument, extension, _private.Piano.onKeysLoaded(canvas));
+		_private.Piano.attachListeners(canvas);
+		return canvas;
+	};
+	
+	_private.Piano.attachListeners = function(canvas) {
+		canvas.addEventListener("mousedown", function(event, point){
+			point = { x: event.clientX-7, y: event.clientY-7 };
+			if (canvas.mouseTarget) {
+				canvas.mouseTarget.release();
+				canvas.mouseTarget = null;
+			}
+			for (var key in canvas.pianoKeys) {
+				if(canvas.pianoKeys.hasOwnProperty(key) && canvas.pianoKeys[key].isMouseHit(point.x, point.y)) {
+					console.log(canvas.pianoKeys[key]);
+					canvas.pianoKeys[key].press();
+					canvas.mouseTarget = canvas.pianoKeys[key];
+					break;
+				}
+			} 
+			canvas.drawPiano();
+		}, true);
+		canvas.addEventListener("mouseup", function(event){
+			if (canvas.mouseTarget) {
+				canvas.mouseTarget.release();
+				canvas.mouseTarget = null;
+			}
+			canvas.drawPiano();
+		}, true);
+		canvas.addEventListener("mouseover", function(){
+			canvas.pianoPower.on = true;
+			canvas.drawPiano();
+		}, true);
+		canvas.addEventListener("mouseout", function(){
+			canvas.pianoPower.on = false;
+			canvas.drawPiano();
+		}, true);
+		_public.addEventListener("keydown", function(event, key){
+			key = event.which.toString();
+			if(canvas.pianoPower.on && _private.Piano.Key.map.hasOwnProperty(key)) {
+				key = _private.Piano.Key.map[key];
+				canvas.pianoKeys[key].press();
+				canvas.drawPiano();
+			}
+		}, true);
+		_public.addEventListener("keyup", function(event, key){
+			key = event.which.toString();
+			if(_private.Piano.Key.map.hasOwnProperty(key)) {
+				key = _private.Piano.Key.map[key];
+				canvas.pianoKeys[key].release();
+				canvas.drawPiano();
+			}
+		}, true);
+	};
+	
+	_private.Piano.drawPiano = function(context, blacks, key) {
+	
+		context = this.getContext("2d");
+		
+		this.pianoBg.draw(context);
+		
+		this.pianoTitle.draw(context);
+		
+		this.pianoPower.draw(context);
+		
+		blacks = [];
+		
+		for (key in this.pianoKeys) {
+			if(this.pianoKeys.hasOwnProperty(key)) {
+				if(key.length == 1) {
+					this.pianoKeys[key].draw(context);
+				} else {
+					blacks.push(this.pianoKeys[key]);
+				}
+			}
+		}
+		
+		for (key = 0; key<blacks.length; key++) {
+			blacks[key].draw(context);
+		}
+		
+	};
+	
+	_private.Piano.onKeysLoaded = function(instance) {
+		return function() {
+			instance.drawPiano();
+		};
+	};
+	
+	_private.Piano.getAudioExtension = function(test, mime) {
+		if (typeof Audio !== undefined) {
+			test = new Audio();
+			mime = test.canPlayType("audio/x-wav");
+			if(mime.length > 0 && mime != "no") { return ".wav"; }
+			mime = test.canPlayType("audio/wav");
+			if(mime.length > 0 && mime != "no") { return ".wav"; }
+			mime = test.canPlayType("audio/ogg");
+			if(mime.length > 0 && mime != "no") { return ".ogg"; }
+		}
+		return null;
+	};
+	
+	_private.Piano.Title = function() {
+		this.top = 20;
+		this.left = 6;
+	};
+	
+	_private.Piano.Title.prototype = {
+		draw: function(context) {
+			context.fillStyle = "#FFF";
+			context.font = "13px 'Times New Roman', Times";
+			context.fillText("HTML5 PIANO", this.left, this.top);
+		}
+	};
+	
+	_private.Piano.Power = function() {
+		this.on = false;
+		this.top = 1;
+		this.left = 94;
+		this.width = this.height = 5;
+	};
+	
+	_private.Piano.Power.prototype = {
+		draw: function(context) {
+			context.fillStyle = this.on ? "#0F0" : "#F00";
+			context.strokeStyle = "#000";
+			context.fillRect(this.left, this.top, this.width, this.height);
+			context.strokeRect(this.left, this.top, this.width, this.height);
+		}
+	};
+	
+	_private.Piano.Background = function() {
+		this.width = this.height = 100;
+	};
+	
+	_private.Piano.Background.prototype = {
+		draw: function(context) {
+			context.fillStyle = "#8A4B08";
+			context.strokeStyle = "#000";
+			context.fillRect(0, 0, this.width, this.height);
+			context.strokeRect(0, 0, this.width, this.height);
+		}
+	};
+	
+	_private.Piano.Key = function(x, y, w, h, fill) {
+		this.x			= x;
+		this.y 			= y;
+		this.width 		= w;
+		this.height		= h;
+		this.pressed 	= false;
+		this.fill 		= fill;
+		this.stroke 	= "#000";
+		this.activeFill = "#CCC";
+	};
+	
+	_private.Piano.Key.map = {
 		"87":  "gs", // w
 		"83":  "a",  // s
 		"69": "bb",  // e
@@ -109,198 +222,91 @@
 		"74":  "f",  // j
 		"73":  "fs", // i
 		"75":  "g"   // k
-	},
-
-	// all the notes the piano can play
-	scale: ["gs","a","bb","b","c","cs","d","eb","e","f","fs","g"],
+	};
 	
-	// the audio mime types supported by piano
-	supportedTypes: { "audio/x-wav": ".wav", "audio/wav": ".wav", "audio/ogg": ".ogg" },
+	_private.Piano.Key.audioFolder = "instruments";
 	
-	// the location of instrument data
-	dataPath: "instruments",
-	
-	// used to test if the current browser is capable of using piano
-	getAudioType: function(test, result) {
-		if (typeof Audio !== "undefined") {
-			test = new Audio("");
-			for (var mime in this.supportedTypes) {
-				if(this.supportedTypes.hasOwnProperty(mime)) {
-					result = test.canPlayType(mime);
-					if(result !== "" && result !== "no") {
-						return this.supportedTypes[mime];
-					}
-				}
-			}
-		}
-		return null;
-	},
-	
-	// gets the piano canvas
-	getPianoCanvas: function(canvas, context) {
-		canvas				= document.createElement("canvas");
-		canvas.style.width	= "100px";
-		canvas.style.height	= "100px";
-		canvas.keyTargets	= {};
-		canvas.active		= false;
-		canvas.scale		= this.scale;
-		canvas.keymap		= this.keymap;
-		canvas.drawPiano	= this.drawPiano;
-		canvas.press		= this.press;
-		canvas.release		= this.release;
-		return canvas;
-	},
-	
-	drawPiano: function(context, i, key, coords, last) {
-		
-		// get the piano coordinates
-		coords = { 
-			white: { 
-				index: 0, 
-				keys: [], 
-				dx: 8,
-				width: 12,
-				height: 60,
-				top: 30
-			}, 
-			black: { 
-				index: 0, 
-				keys: [], 
-				dx: 5,
-				width: 7,
-				height: 33,
-				top: 30
-			}, 
-			bg: {
-				width: 100,
-				height: 100
-			},
-			title: {
-				top: 20,
-				left: 6
-			}, 
-			active: {
-				top: 1,
-				left: 94,
-				width: 5,
-				height: 5
-			}
+	_private.Piano.Key.getKeys = function(instrument, extension, callback, keyData, keys, key) {
+		keys = {};
+		instrument = _private.Piano.Key.audioFolder + "/" + instrument + "/";
+		keyData = {
+			callback: callback, 
+			queue: 0,
+			last: null,
+			top: 30,
+			white: { dx: 8, width: 12, height: 60 }, 
+			black: { dx: 5, width: 6, height: 33 }
 		};
-		
-		// setup stage
-		this.width = coords.bg.width;
-		this.height = coords.bg.height;
-		context = this.getContext("2d");
-		
-		// draw background
-		context.fillStyle = "#8A4B08";
-		context.strokeStyle = "#000";
-		context.fillRect(0, 0, coords.bg.width, coords.bg.height);
-		context.strokeRect(0, 0, coords.bg.width, coords.bg.height);
-		
-		// draw title
-		context.fillStyle = "#FFF";
-		context.font = "13px 'Times New Roman', Times";
-		context.fillText("HTML5 PIANO", coords.title.left, coords.title.top);
-		
-		if(this.mouseTarget && !this.keyTargets[this.mouseTarget]) { 
-			this.release(this.mouseTarget);
-			this.mouseTarget = null;
-		}
-		
-		for (i=0;i<this.scale.length;i++) {
-			key = this.scale[i];
-			if (key.length == 1) {
-				if(this.mouse && !this.mouseTarget && 
-					this.mouse.x >= coords.white.dx &&
-					this.mouse.x <= coords.white.dx+coords.white.width &&
-					this.mouse.y >= coords.white.top &&
-					this.mouse.y <= coords.white.top+coords.white.height) {
-						this.press(this.mouseTarget = key);
+		for (var i in _private.Piano.Key.map) {
+			if(_private.Piano.Key.map.hasOwnProperty(i)) {
+				key = _private.Piano.Key.map[i];
+				if (key.length === 1) {
+					keys[key] = new _private.Piano.Key(
+						keyData.white.dx, 
+						keyData.top, 
+						keyData.white.width, 
+						keyData.white.height,
+						"#FFF");
+					keyData.white.dx += keyData.white.width; 
+					if(keyData.last === "w") { keyData.black.dx += keyData.white.width; }
+					keyData.last = "w";
+				} else {
+					keys[key] = new _private.Piano.Key(
+						keyData.black.dx, 
+						keyData.top, 
+						keyData.black.width, 
+						keyData.black.height,
+						"#000");
+					keyData.black.dx += keyData.white.width;
+					keyData.last = "b";
 				}
-				coords.white.keys.push({
-					x: coords.white.dx, 
-					pressed: (this.keyTargets[key] || this.mouseTarget == key)
-				});
-				coords.white.dx += coords.white.width; 
-				if(last === "w") { coords.black.dx += coords.white.width; }
-				last = "w";
-			} else {
-				if(this.mouse && !this.mouseTarget && 
-					this.mouse.x >= coords.black.dx &&
-					this.mouse.x <= coords.black.dx+coords.black.width &&
-					this.mouse.y >= coords.black.top &&
-					this.mouse.y <= coords.black.top+coords.black.height) {
-						this.press(this.mouseTarget = key);
-				}
-				coords.black.keys.push({
-					x: coords.black.dx, 
-					pressed: (this.keyTargets[key] || this.mouseTarget == key)
-				});
-				coords.black.dx += coords.white.width;
-				last = "b";
-			} 
-			
+				keyData.queue++;
+				keys[key].bindAudio(
+					instrument + key + extension,
+					_private.Piano.Key.onKeyLoaded(keyData)
+				);
+			}
 		}
-		
-		// draw the white keys
-		for (i=0;i<coords.white.keys.length;i++) {
-			key = coords.white.keys[i];
-			context.fillStyle = key.pressed ? "#CCC" : "#FFF";
-			context.strokeStyle = "#000";
-			context.fillRect(key.x, coords.white.top, coords.white.width, coords.white.height);
-			context.strokeRect(key.x, coords.white.top, coords.white.width, coords.white.height);
+		return keys;
+	};
+	
+	_private.Piano.Key.onKeyLoaded = function(data) {
+		return function() {
+			if(--data.queue === 0) { data.callback(); }
+		};
+	};
+	
+	_private.Piano.Key.prototype = {
+		draw: function(context) {
+			context.fillStyle = this.pressed ? this.activeFill : this.fill;
+			context.strokeStyle = this.stroke;
+			context.fillRect(this.x, this.y, this.width, this.height);
+			context.strokeRect(this.x, this.y, this.width, this.height);
+		},
+		bindAudio: function(source, callback, key) {
+			key				= this;
+			key.audio 		= new Audio();
+			key.audio.src 	= source;
+			key.audio.addEventListener("canplaythrough", function onCanPlayThrough() {
+				key.audio.removeEventListener("canplaythrough", onCanPlayThrough, true);
+				callback(key.audio);
+			}, true);
+			key.audio.load();
+		},
+		press: function() {
+			if(!this.pressed) {
+				this.audio.pause();
+				this.audio.load();				
+				this.audio.play();
+				this.pressed = true;
+			}
+		},
+		release: function() {
+			this.pressed = false;
+		},
+		isMouseHit: function(x, y) {
+			return (x >= this.x) && (y >= this.y) && (x <= this.x + this.width) && (y <= this.y + this.height);
 		}
-		
-		// draw the black keys
-		for (i=0;i<coords.black.keys.length;i++) {
-			key = coords.black.keys[i];
-			context.fillStyle = key.pressed ? "#CCC" : "#000";
-			context.fillRect(key.x, coords.black.top, coords.black.width, coords.black.height);
-		}
-		
-		// draw the active indicator
-		context.fillStyle = this.active ? "#0F0" : "#F00";
-		context.strokeStyle = "#000";
-		context.fillRect(coords.active.left, coords.active.top, coords.active.width, coords.active.height);
-		context.strokeRect(coords.active.left, coords.active.top, coords.active.width, coords.active.height);
+	};
 	
-	},
-	
-	// called to load the audio into memory
-	loadAudio: function(instrument, type, callback, audio, key, path, queue,i) {	
-		queue = 0; audio = {};
-		for (i=0;i<this.scale.length;i++) {
-			queue++;
-			key			= this.scale[i];
-			path		= this.dataPath + "/" + instrument + "/";
-			audio[key]	= new Audio("");
-			audio[key].addEventListener("canplaythrough", (function(key){
-				return function onCanPlay() {
-					key.removeEventListener("canplaythrough", onCanPlay, true);
-					if(--queue === 0) { callback(); }
-				};
-			})(audio[key]), true);
-			audio[key].src = path + key + type;
-			audio[key].load();
-		}
-		return audio;
-	},
-	
-	// emulates pressing a key
-	press: function(key) {
-		key = this.audio[key];
-		if(!key.pressed) {
-			key.pause();
-			key.load();				
-			key.play();
-			key.pressed = true;
-		}
-	},
-	
-	// emulates releasing a key
-	release: function(key) {
-		this.audio[key].pressed = false;
-	}
-	
-}, window);
+})(window, {});
